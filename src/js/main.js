1a6b9d47fc811ac4536d327afd80987e6c289940
Banchee501 <rosstactical373@gmail.com>
2022-09-28 20:57:38 +0300

    add sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import moduls from './modules/moduls';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
 
     moduls();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
 });
