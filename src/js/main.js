5c3f2888098594ffd8411586f323fcb0cfc1d21b
Banchee501 <rosstactical373@gmail.com>
2022-09-29 15:45:14 +0300

    add forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import moduls from './modules/moduls';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,4 +9,5 @@ window.addEventListener('DOMContentLoaded', () => {
     moduls();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
 });
